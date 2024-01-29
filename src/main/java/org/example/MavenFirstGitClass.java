0b6883789e754992bfd66a2623ea76c34800bbdc
Raj16012024 <mrsrajee@yahoo.co.uk>
2024-01-29 20:08:52 +0000

    Fourth Commit and push

--- src/main/java/org/example/MavenFirstGitClass.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class MavenFirstGitClass {
-    public static void main (String[] args) {
-
-        System.out.println("Hello and Welcome to My First Maven Git Class");
-    }
-}
